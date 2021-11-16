c033c16de10fc24a8ce40e87241eaeb06a4edd98
Hannes Mehnert <hannes@mehnert.org>
2021-11-16 14:03:09 +0100

    happy-eyeballs-{lwt,mirage}: add ?happy_eyeballs and ?dns optional arguments

--- mirage/happy_eyeballs_mirage.mli
+++ mirage/happy_eyeballs_mirage.mli
@@ -1,8 +1,10 @@
 module Make (R : Mirage_random.S) (T : Mirage_time.S) (C : Mirage_clock.MCLOCK) (P : Mirage_clock.PCLOCK) (S : Mirage_stack.V4V6) : sig
+  module DNS : module type of Dns_client_mirage.Make(R)(T)(C)(P)(S)
+
   type t
 
-  val create : ?aaaa_timeout:int64 -> ?connect_timeout:int64 ->
-    ?resolve_timeout:int64 -> ?timer_interval:int64 -> S.t -> t
+  val create : ?happy_eyeballs:Happy_eyeballs.t ->
+    ?dns:DNS.t -> ?timer_interval:int64 -> S.t -> t
 
   val connect_host : t -> [`host] Domain_name.t -> int list ->
     ((Ipaddr.t * int) * S.TCP.flow, [ `Msg of string ]) result Lwt.t
