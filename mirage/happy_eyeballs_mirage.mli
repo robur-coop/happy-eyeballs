e4000712630827739e0981f8501af6c0b7613898
Romain Calascibetta <romain.calascibetta@gmail.com>
2022-02-12 13:38:05 +0100

    Fix constraints on type across projects

--- mirage/happy_eyeballs_mirage.mli
+++ mirage/happy_eyeballs_mirage.mli
@@ -1,11 +1,14 @@
 module type S = sig
-  module DNS : Dns_client_mirage.S
+  module Transport : Dns_client.S
+    with type io_addr = [ `Plaintext of Ipaddr.t * int  | `Tls of Tls.Config.client * Ipaddr.t * int ]
+     and type +'a io = 'a Lwt.t
 
   type t
+  type dns
   type flow
 
   val create : ?happy_eyeballs:Happy_eyeballs.t ->
-    ?dns:DNS.t -> ?timer_interval:int64 -> DNS.Transport.stack -> t
+    ?dns:dns -> ?timer_interval:int64 -> Transport.stack -> t
 
   val connect_host : t -> [`host] Domain_name.t -> int list ->
     ((Ipaddr.t * int) * flow, [> `Msg of string ]) result Lwt.t
@@ -23,11 +26,12 @@ module type S = sig
     ?resolve_timeout:int64 ->
     ?resolve_retries:int ->
     int64 ->
-    DNS.t ->
-    ?timer_interval:int64 -> DNS.Transport.stack -> t Lwt.t
+    dns ->
+    ?timer_interval:int64 -> Transport.stack -> t Lwt.t
 end
 
 module Make (T : Mirage_time.S) (C : Mirage_clock.MCLOCK) (S : Tcpip.Stack.V4V6)
   (DNS : Dns_client_mirage.S with type Transport.stack = S.t)
-  : S with type DNS.Transport.stack = S.t
+  : S with module Transport = DNS.Transport
+       and type dns = DNS.t
        and type flow = S.TCP.flow
