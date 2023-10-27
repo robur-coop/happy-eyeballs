84e340ac3c3442f1693a59a0ff8f2465880b933d
Calascibetta Romain <romain.calascibetta@gmail.com>
2023-10-27 16:51:54 +0200

    Reverse the dependency between dns, dns-client-lwt and happy-eyeballs-lwt and use an injection mechanism to resolve domain name instead of a direct usage of dns-client-lwt

--- lwt/happy_eyeballs_lwt.mli
+++ lwt/happy_eyeballs_lwt.mli
@@ -1,11 +1,17 @@
 (** The type of the abstract state of happy eyeballs. *)
 type t
 
-val create : ?happy_eyeballs:Happy_eyeballs.t -> ?dns:Dns_client_lwt.t ->
+type getaddrinfo = {
+  getaddrinfo : 'response 'a.
+    'response Dns.Rr_map.key -> 'a Domain_name.t -> ('response, [ `Msg of string ]) result Lwt.t
+} [@@unboxed]
+
+val create : ?happy_eyeballs:Happy_eyeballs.t ->
+  ?getaddrinfo:getaddrinfo ->
   ?timer_interval:int64 -> unit -> t
-(** [create ~happy_eyeballs ~dns ~timer_interval ()] creates an initial state
-    of happy eyeballs with the specified timeouts in nanoseconds - the default
-    for [timer_interval] is [Duration.of_ms 10]. *)
+(** [create ~happy_eyeballs ~getaddrinfo ~timer_interval ()] creates an initial
+    state of happy eyeballs with the specified timeouts in nanoseconds - the
+    default for [timer_interval] is [Duration.of_ms 10]. *)
 
 val connect_host : t -> [`host] Domain_name.t -> int list ->
   ((Ipaddr.t * int) * Lwt_unix.file_descr, [ `Msg of string ]) result Lwt.t
@@ -26,3 +32,5 @@ val connect : t -> string -> int list ->
     may be a host name, or an IP address.
 
     @raise Failure if [ports] is the empty list. *)
+
+val inject : getaddrinfo:getaddrinfo -> t -> unit
